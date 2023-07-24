03c9ddb7df8410ab1b57f3c74d77dfa0c3eb3628
Mugema <54482161+Mugema@users.noreply.github.com>
2023-07-24 18:35:23 +0300

    Update Q2.kt

--- src/main/kotlin/Q2.kt
+++ src/main/kotlin/Q2.kt
@@ -1,3 +1,12 @@
 fun main(){
+    println("Enter the distance")
+    val d:Double= readln().toDouble()
+
+    println("Enter the Speed")
+    val s:Double= readln().toDouble()
+
+    val time:Double=d/s
+    println("The time it takes to cover a distance of $d at a speed of $s is $time seconds")
+
     
 }
